a2d2569e723462e1c774945624fc6e82f4f6673f
NataliaMalejka <natalia280803@gmail.com>
2024-10-07 20:13:07 +0200

    Game Over Panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject gameOverPanel;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0);
         }
+    }
+    public void LoadGame(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -7,15 +7,19 @@ public class cameraController : MonoBehaviour
     [SerializeField]
     private Vector3 distance;
     private GameObject DragoObject;
+    private GameManager gameManager;
 
     private void Start()
     {
         DragoObject = GameObject.FindGameObjectWithTag("Player");
+        gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
     private void Update()
     {
-        if(DragoObject != null)     
+        if (DragoObject != null)
             transform.position = new Vector3(DragoObject.transform.position.x, 0, 0) + distance;
+        else
+            gameManager.GameOver();
     }
 }
